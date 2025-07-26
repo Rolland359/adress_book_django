d98610805aff0c7e3bcb4d10925cce04fe1fc16c
Rolland359 <rolland@futurmap.com>
2025-07-26 14:09:07 +0300

    maj du formulaire add et create et fonction general process

--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse
 from .models import Contact
 from django.http import HttpResponseRedirect
 # Create your views here.
@@ -7,7 +8,6 @@ def index(request):
     contacts = get_list_or_404(Contact)
     context = {'contact_list': contacts}
     return render(request, "book/index.html", context)
-
 
 def details(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id)
@@ -22,8 +22,27 @@ def update(request, contact_id):
     context = {"contact":contact}
     return render(request, "book/update.html", context)
 
-def process_add(request):
-    print("utiliseateur sauvegardé")
+def general_process(request):
+    type_daction = request.POST.get("action")
+    if type_daction == "add":
+
+        new_contact = Contact()
+
+        new_contact.name = request.POST.get("name")
+        new_contact.lastname = request.POST.get("lastname")
+        new_contact.phone = request.POST.get("phone")
+        new_contact.email = request.POST.get("email")
+        new_contact.email = request.POST.get("email")
+        new_contact.address = request.POST.get("email")
+        new_contact.photo = request.POST.get("photo")
+        new_contact.save()
+
+        return HttpResponseRedirect(reverse("book:index", args=()))
+    
+    elif type_daction == "update":
+        return HttpResponseRedirect(reverse("book:index", args=()))
 
 def process_delete(request, contact_id):
-    pass
+    contact = get_object_or_404(Contact, pk=contact_id)
+    context = {"contact":contact, "message":"delete"}
+    return render(request, "book/update.html", context)
